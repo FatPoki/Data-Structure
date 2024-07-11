91c274092864582ddd4b0c745d710fcdeaa92558
Anoop sharma <75870938+FatPoki@users.noreply.github.com>
2024-07-12 01:17:46 +0530

    Update smallest.js

--- smallest.js
+++ smallest.js
@@ -1,1 +1,22 @@
+// In this smallest.js file i have created a algo that will find smallest value from the given array in js .
 
+function findSmallest(arr) {
+    if (arr.length === 0) {
+      return null; 
+    }
+  
+    let smallest = arr[0];
+  
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < smallest) {
+        smallest = arr[i];
+      }
+    }
+  
+    return smallest;
+  }
+  
+  
+  let numbers = [20,19,10,92,9,18,45,67,33,20,10,9];
+  console.log(findSmallest(numbers)); 
+  
